1c788f2dc205592bb68330a50b47cc9a4a7b9417
CodeSasuke <141392657+CodeSasuke@users.noreply.github.com>
2024-10-11 03:10:31 +0530

    Time: 4 ms (100%), Space: 44 MB (37.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
         int rev = 0;
         int dum = x;
         while(dum > 0){
